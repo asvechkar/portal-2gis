ca22f9e9d136853cf9d4c8122f36b72a3a0d766b
Aleksey Svechkar <asvechkar@gmail.com>
2013-07-09 19:42:55 +0400

    master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portal2gis::Application.config.secret_key_base = '615e16bd6f9565a2b95bd827b2b5fe4e9d2576a693ff2ba9285ca715918815032bf32b0d61cb25292352d931b140c2d4bfa9d9ef6ec161c3d082789c1f55e7bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Portal2gis::Application.config.session_store :cookie_store, key: '_portal-2gis_session'
