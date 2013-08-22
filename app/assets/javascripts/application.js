e32f6c608ad2db7151a2811d097dda8072b10701
Aleksey Svechkar <asvechkar@gmail.com>
2013-08-22 19:34:22 +0400

    add script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,3 +11,45 @@
 //= require holder
 //= require jquery.uniform
 
+function toggleMenuHidden()
+{
+  //console.log('toggleMenuHidden');
+  $('.container-fluid:first').toggleClass('menu-hidden');
+  $('#menu').toggleClass('hidden-phone', function()
+  {
+    if ($('.container-fluid:first').is('.menu-hidden'))
+    {
+      if (typeof resetResizableMenu != 'undefined')
+        resetResizableMenu(true);
+    }
+    else
+    {
+      removeMenuHiddenPhone();
+
+      if (typeof lastResizableMenuPosition != 'undefined')
+        lastResizableMenuPosition();
+    }
+
+    if (typeof $.cookie != 'undefined')
+      $.cookie('menuHidden', $('.container-fluid:first').is('.menu-hidden'));
+  });
+
+  if (typeof masonryGallery != 'undefined')
+    masonryGallery();
+}
+
+function removeMenuHiddenPhone()
+{
+  if (!$('.container-fluid:first').is('.menu-hidden') && $('#menu').is('.hidden-phone'))
+    $('#menu').removeClass('hidden-phone');
+}
+
+$(function()
+{
+  $('.navbar.main .btn-navbar').click(function()
+  {
+    var disabled = typeof toggleMenuButtonWhileTourOpen != 'undefined' ? toggleMenuButtonWhileTourOpen(true) : false;
+    if (!disabled)
+      toggleMenuHidden();
+  });
+});
