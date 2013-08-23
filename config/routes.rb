e516f790c6e590378de6b82da8acb37ecb434409
Aleksey Svechkar <asvechkar@gmail.com>
2013-08-23 16:38:49 +0400

    add cities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Portal2gis::Application.routes.draw do
+  resources :cities
+
   devise_for :users, :controllers => { :registrations => 'registrations', :sessions => 'sessions', :passwords => 'passwords' }
   root 'portal#index'
 end
