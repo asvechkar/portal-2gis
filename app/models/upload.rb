0996341e1deb7ac7792453c03eb886b18aa18dcb
Alexander Kleymenov <unit@notarikon.net>
2013-08-27 16:28:09 +0400

    portal2gis-57 Импорт справочника Клиентов из файла "Выписка по лицевым счетам"

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,6 +6,7 @@ class Upload < ActiveRecord::Base
 
   def to_jq_upload
     {
+        "id" => self.id,
         "name" => read_attribute(:upload_file_name),
         "size" => read_attribute(:upload_file_size),
         "url" => upload.url(:original),
