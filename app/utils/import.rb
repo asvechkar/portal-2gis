0753e833be679ad949fa83a98ff62d9bb37442df
Alexander Kleymenov <unit@notarikon.net>
2013-08-28 15:33:15 +0400

    portal2gis-58 Импорт Бланков заказа из файла PlanningReport

--- app/utils/import.rb
+++ app/utils/import.rb
@@ -3,12 +3,13 @@ module Import
   def self.xlsx(file, cname, uid)
     sheet = Roo::Excelx.new(file)
     case
-      when cname == 'client' then Import.client(sheet, uid)
+      when cname == 'client' then Import.clients(sheet, uid)
+      when cname == 'order' then Import.orders(sheet, uid)
     end
   end
 
 
-  def self.client(sheet, uid)
+  def self.clients(sheet, uid)
     sheet.last_row.downto(2) { |list|
       (sheet.cell('I', list).to_s.size < 1) ? code = 'NONE' :
           code = sheet.cell('I', list).to_s.slice!(/(^[^,]*)/)
@@ -18,5 +19,16 @@ module Import
                     :inn => inn,
                     :user_id => uid) unless Client.where(:inn => inn).first && inn.size < 1
     }
+  end
+
+  def self.orders(sheet, uid)
+    sheet.last_row.downto(2) { |list|
+
+    }
+
+  end
+
+  def self.employ
+
   end
 end
