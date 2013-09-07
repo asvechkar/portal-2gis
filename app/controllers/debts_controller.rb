0ce641e6e4ac8a14703c6f31da5ae6d35fb40f39
Alexander Kleymenov <unit@notarikon.net>
2013-09-07 14:06:47 +0400

    portal2gis-59 Импорт Задолжености из файла PlanningReport

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -62,6 +62,11 @@ class DebtsController < ApplicationController
     end
   end
 
+
+  def floating
+    redirect_to debts_path
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_debt
