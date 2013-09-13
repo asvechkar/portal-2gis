07250e6c99b416504cc3a3520441e1c01830e802
Aleksey Svechkar <asvechkar@gmail.com>
2013-09-13 13:38:41 +0400

    add total by group

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,6 +6,8 @@ class ReportsController < ApplicationController
 
   def planfact
     # @report_data = Employee.all.map{|employee| employee.groups.first.nil? ? nil : employee}.compact
-    @report_data = Employee.all.delete_if{|employee| employee.groups.first.nil?}
+    # @report_data = Employee.all.delete_if{|employee| employee.groups.first.nil?}
+    @groups = Group.all
+    @employees = Employee.all
   end
 end
