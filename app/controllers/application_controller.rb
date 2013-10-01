1155f55262d8e3870a3ad09d43933620d2536df0
Aleksey Svechkar <asvechkar@gmail.com>
2013-10-01 23:11:59 +0400

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,10 @@ class ApplicationController < ActionController::Base
   end
   
   def after_sign_in_path_for(resource)
-    #
+    root_path
   end
   
   def after_sign_up_path_for(resource)
-    #
+    root_path
   end
 end
