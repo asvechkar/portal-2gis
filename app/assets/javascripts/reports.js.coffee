bd934898bb53bb4117b38b6e652866d84a763dd5
Aleksey verbivskyy <verbaleks@gmail.com>
2013-10-30 11:03:25 +0200

    fixed planfacts formuls

--- app/assets/javascripts/reports.js.coffee
+++ app/assets/javascripts/reports.js.coffee
@@ -3,3 +3,5 @@ $ ->
     event.preventDefault()
     $('#update_planfact').val('1')
     $('form#planfact_params').submit()
+
+  $('a[rel=tooltip]').tooltip({html:true}).click (e) -> e.preventDefault()
--- app/models/planfact.rb
+++ app/models/planfact.rb
@@ -1,7 +1,5 @@
 class Planfact < ActiveRecord::Base
-  belongs_to :planfactable, polymorphic: true
 
-  #validates_presence_of :report_date, :clients_plan, :clients_fact, :weight_plan, :weight_fact, :income_plan, :income_fact,
-  #:pro_percent, :employee_ic, :group_ic, :branch_ic
+  belongs_to :planfactable, polymorphic: true
 
 end
